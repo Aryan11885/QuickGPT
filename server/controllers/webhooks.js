6afa0024b9ff31b2f1c1f2a3e0e76b2b27ef7c0b
Aryan Tripathi <aryanapril4@gmail.com>
2025-09-18 09:25:17 +0530

    fix: corrected transactionId in Stripe webhook

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -23,7 +23,7 @@ export const stripeWebhooks = async (request, response) => {
         })
 
         const session = sessionList.data[0];
-        const {transaction, appId} = session.metadata;
+        const {transactionId, appId} = session.metadata;
 
         if(appId === "quickgpt") {
           const transaction = await Transaction.findOne({_id: transactionId, isPaid: false})
